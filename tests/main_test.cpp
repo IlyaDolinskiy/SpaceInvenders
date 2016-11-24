3efc5d67d8f9edaea64c98f5d5fddf8917bd74ca
Dmitry Ivanov <ivanovdmitrysrgeevich8@gmail.com>
2016-11-24 20:04:32 +0300

    added Logger

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,9 +1,13 @@
 #include "gtest/gtest.h"
 
+#include <string>
+
 #include "GameObjects/GameObjects.hpp"
+#include "System/Logger/Logger.hpp"
 
 TEST(startA, main_testA)
 {
   Player.do_smth();
   EXPECT_EQ(Player.m_i, 2);
+  Log(Logger::Info) << std::string("msg");
 }
